f21907e615e533c945a4cc49eba66daba9f4f025
Mensch272 <47662901+mensch272@users.noreply.github.com>
2021-09-04 00:43:08 +0500

    updated wuxiasite.py

--- README.md
+++ README.md
@@ -142,7 +142,7 @@ A collection of novel sources offering varying amounts of scraping capability.
             <td align="center">en</td>
             <td>https://wuxiaworld.site</td>
             <td align="center"></td>
-            <td align="center"></td>
+            <td align="center">2021-09-03</td>
         </tr>
         <tr>
             <td align="center">en</td>
--- novelsave_sources/sources/novel/wuxiasite.py
+++ novelsave_sources/sources/novel/wuxiasite.py
@@ -1,18 +1,19 @@
-from typing import List, Tuple
+import datetime
+from urllib.parse import parse_qs, urlparse
+
+from bs4 import BeautifulSoup
 
 from .source import Source
-from ...models import Chapter, Novel, Metadata
+from ...models import Chapter, Volume, Novel, Metadata
 
 
 class WuxiaSite(Source):
     name = 'WuxiaWorld.site'
     base_urls = ('https://wuxiaworld.site',)
+    last_updated = datetime.date(2021, 9, 3)
 
-    def novel(self, url: str) -> Tuple[Novel, List[Chapter], List[Metadata]]:
+    def novel(self, url: str) -> Novel:
         soup = self.get_soup(url)
-        metadata = []
-
-        # ---- info ----
 
         title = ' '.join([
             str(x)
@@ -29,28 +30,43 @@ class WuxiaSite(Source):
         novel = Novel(
             title=title,
             author=author,
-            thumbnail_url=soup.select_one('.summary_image a img')['src'],
-            synopsis='\n'.join(p.text.strip() for p in soup.select('.summary__content > p')),
+            thumbnail_url=soup.select_one('.summary_image a img')['srcset'].split(' ', maxsplit=1)[0],
+            synopsis=[str(p).strip() for p in soup.select_one('.summary__content > p:nth-child(2)').find_all(text=True, recursive=False)],
             url=url,
         )
 
+        for a in soup.select('.artist-content > a'):
+            novel.metadata.append(Metadata('contributor', a.text.strip(), others={'role': 'illustrator'}))
+
         for a in soup.select('a[href*="genre"][rel="tag"]'):
-            metadata.append(Metadata('subject', a.text.strip()))
+            novel.metadata.append(Metadata('subject', a.text.strip()))
 
-        # ---- chapters ----
+        shortlink = soup.select_one('link[rel="shortlink"]')['href']
+        novel_id = int(parse_qs(urlparse(shortlink).query).get('p')[0])
+        response = self.request(
+            'POST',
+            'https://wuxiaworld.site/wp-admin/admin-ajax.php',
+            data={
+                'action': 'manga_get_chapters',
+                'manga': novel_id,
+            }
+        )
 
-        chapters = []
-        for a in reversed(soup.select('ul.main li.wp-manga-chapter a')):
-            index = len(chapters)
+        soup = BeautifulSoup(response.content, 'lxml')
+
+        volume = Volume.default()
+        novel.volumes.append(volume)
+        for a in reversed(soup.select('li.wp-manga-chapter a')):
+            index = len(volume.chapters)
             chapter = Chapter(
                 index=index,
                 title=a.text.strip() or f'Chapter {index}',
                 url=a['href'],
             )
 
-            chapters.append(chapter)
+            volume.chapters.append(chapter)
 
-        return novel, chapters, metadata
+        return novel
 
     def chapter(self, chapter: Chapter):
         soup = self.get_soup(chapter.url)

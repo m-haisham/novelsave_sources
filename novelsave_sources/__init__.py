da674b9494146690cc4dd4e82f777879999ffba7
Mensch272 <47662901+mensch272@users.noreply.github.com>
2021-09-09 23:44:54 +0500

    bump version to 0.2.0

--- novelsave_sources/__init__.py
+++ novelsave_sources/__init__.py
@@ -1,11 +1,9 @@
-__version__ = '0.1.1'
-
-from .models import Novel, Volume, Chapter, Metadata
+__version__ = '0.2.0'
 
 from .exceptions import (
     SourcesException, BadResponseException, UnknownSourceException,
     UnavailableException, ChapterException,
 )
-
+from .models import Novel, Volume, Chapter, Metadata
 from .sources import sources, meta_sources
 from .utils.parsers import parse_source, parse_metasource

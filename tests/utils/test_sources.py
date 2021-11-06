af797362b0224c3cae3076d0f167997552c8c5d5
Mensch272 <47662901+mensch272@users.noreply.github.com>
2021-11-06 21:25:55 +0500

    Removed specific source usage in tests

--- tests/utils/test_sources.py
+++ tests/utils/test_sources.py
@@ -10,17 +10,14 @@ from novelsave_sources import (
     UnknownSourceException,
 )
 from novelsave_sources.sources import MetaSource, Source
-from novelsave_sources.sources.metadata.novelupdates import NovelUpdates
-from novelsave_sources.sources.novel.webnovel import Webnovel
 
 
 def test_novel_source_types():
     sources = novel_source_types()
+    assert len(sources) != 0
+
     for source in sources:
         assert issubclass(source, Source)
-
-    assert Webnovel in sources
-    assert NovelUpdates not in sources
 
 
 def test_locate_novel_source(mocker):
@@ -40,11 +37,10 @@ def test_locate_novel_source(mocker):
 
 def test_metadata_source_types():
     sources = metadata_source_types()
+    assert len(sources) != 0
+
     for source in sources:
         assert issubclass(source, MetaSource)
-
-    assert Webnovel not in sources
-    assert NovelUpdates in sources
 
 
 def test_locate_metadata_source(mocker):
